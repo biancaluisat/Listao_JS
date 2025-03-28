2979019cbc776b9cb4d6b90d668116f6b1647e5c
Bianca Luisa <ghostekkjk@gmail.com>
2025-03-28 11:43:24 -0300

    feat(if-else): adiciona verificação de positivo, negativo ou zero

---
+++ If_Else/ativei.js
@@ -0,0 +1,9 @@
+let numr = 0;
+
+if (numr > 0){
+    console.log('O número é maior que 0 e positivo.');
+}else if (numr == 0){
+    console.log('O número é 0.');
+}else {
+    console.log('O número é negativo.');
+}
