17b8662f197b265b4a33791d03b11899f4cc4d09
Bianca <ghostekkjk@gmail.com>
2025-03-28 16:25:29 -0300

    feat(if-else): adiciona identificação do menor entre três números

---
+++ If_Else/ativie8.js
@@ -0,0 +1,11 @@
+let numr = 5;
+let numr2 = 2;
+let numr3 = 20;
+
+if (numr < numr2 && numr < numr3){
+    console.log('O primeiro número é o menor. ✅');
+}else if (numr2 < numr && numr2 < numr3){
+    console.log('O número 2 é o menor.✅');
+}else{
+    console.log('O 3 número é o menor. ✅')
+}
