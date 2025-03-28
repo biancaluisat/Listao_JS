0fd59a142cb24282938c3bd1e4f06b90c46503b9
Bianca <ghostekkjk@gmail.com>
2025-03-28 16:10:57 -0300

    feat(if-else): adiciona verificação de aprovação por nota

---
+++ If_Else/ativie5.js
@@ -0,0 +1,9 @@
+let nota = 45;
+
+if (nota >=0 && nota <= 60){
+    console.log('Reprovado ❌');
+}else if (nota > 60 && nota <= 100){
+    console.log('Aprovado ✅');
+}else {
+    console.log('Nota inválida.');
+}
