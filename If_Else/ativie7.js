76262bb4153413cb10da92bd8d622e1bc4ccff93
Bianca <ghostekkjk@gmail.com>
2025-03-28 16:21:24 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

---
+++ If_Else/ativie7.js
@@ -0,0 +1,7 @@
+let salário = 4000;
+
+if (salário > 2000)
+    console.log(`${salário * 0.90} é o salário com desconto de 10%.`);
+else{
+    console.log(`${salário * 0.95} é o salário com desconto de 5%`);
+}
