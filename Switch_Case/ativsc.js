e80ed616d04fab5de43e4b82df657e1719983a27
Bianca <ghostekkjk@gmail.com>
2025-03-31 08:13:12 -0300

    feat(switch-case): adiciona exibição do dia da semana

---
+++ Switch_Case/ativsc.js
@@ -0,0 +1,27 @@
+let  numr = 4
+
+switch(numr){
+    case 1:
+        console.log('Hoje é Domingo!');
+        break;
+    case 2:
+        console.log('Hoje é Segunda!');
+        break;
+    case 3:
+        console.log('Hoje é Terça!');
+        break;
+    case 4:
+        console.log('Hoje é Quarta!');
+        break;
+    case 5:
+        console.log('Hoje é Quinta!');
+        break;
+    case 6:
+        console.log('Hoje é Sexta!');
+        break;
+    case 7:
+        console.log('Hoje é Sábado!');
+        break;
+    default:
+        console.log('Dia Inválido.');  
+}
