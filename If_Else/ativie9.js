764adb1ac8916a088852a7d071b5aa686ff9565c
Bianca <ghostekkjk@gmail.com>
2025-03-31 07:54:06 -0300

    feat(if-else): adiciona classificação de clima por temperatura

---
+++ If_Else/ativie9.js
@@ -0,0 +1,9 @@
+let tempeturatura = 7;
+
+if (tempeturatura < 15){
+    console.log('Está frio ❄️');
+}else if (tempeturatura >= 15 && tempeturatura <= 25){
+    console.log('Esta agradável 🌡️');
+}else {
+    console.log('Está quente ☀️');
+}
