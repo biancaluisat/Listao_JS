e9a31b74b193bdf9edd6bb943a48a744bfc5a97a
Bianca <ghostekkjk@gmail.com>
2025-03-31 09:10:27 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

--- Operadores_Lógicos/ativl3.js
+++ Operadores_Lógicos/ativl3.js
@@ -5,5 +5,5 @@ let numr3 = 101;
 if (numr && numr2 && numr3 >100){
     console.log('Algum dos numeros passa de 100.');
 }else {
-    console.log('Nenhum passa de 100');
+    console.log('Nenhum passa de 100.');
 }
