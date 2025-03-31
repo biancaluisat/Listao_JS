9accd1bfae468cb56efe2a6ce164fcc70d84fa3b
Bianca <ghostekkjk@gmail.com>
2025-03-31 08:46:39 -0300

    feat(switch-case): adiciona exibição da estação do ano

--- Switch_Case/ativsc5.js
+++ Switch_Case/ativsc5.js
@@ -1,1 +1,18 @@
-let estacao = 
+let estacao =  2;
+
+switch(estacao){
+    case 1:
+        console.log('É Verão!');
+        break;
+    case 2:
+        console.log('É Outono!');
+        break;
+    case 3:
+        console.log('É Inverno!');
+        break;
+    case 4:
+        console.log('É Primevera!');
+        break;
+    default:
+        console.log('Estação inválida.');
+}
