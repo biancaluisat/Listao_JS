a7135b5ea57d4406b43d34cf92a900a5c6dff971
Bianca <ghostekkjk@gmail.com>
2025-03-31 08:24:16 -0300

    feat(switch-case): adiciona exibição do mês do ano

---
+++ Switch_Case/ativsc2.js
@@ -0,0 +1,42 @@
+let mes = 9;
+
+switch (mes){
+    case 1:
+        console.log('É Janeiro!');
+        break;
+    case 2:
+        console.log('É Fevereiro!');
+        break;
+    case 3:
+        console.log('É Março!');
+        break;
+    case 4:
+        console.log('É Abril!');
+        break;
+    case 5:
+        console.log('É Maio!');
+        break;
+    case 6:
+        console.log('É Junho!');
+        break;
+    case 7:
+        console.log('É Julho!');
+        break;
+    case 8:
+        console.log('É Agosto!');
+        break;
+    case 9:
+        console.log('É Setembro!');
+        break;
+    case 10:
+        console.log('É Outubro!');
+        break;
+    case 11:
+        console.log('É Novembro!');
+        break;
+    case 12:
+        console.log('É Dezembro!');
+        break;
+    default:
+        console.log('Mês inválido.');
+}
