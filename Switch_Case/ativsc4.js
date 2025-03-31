80b7c54ae4c40925995ff9e161aa9c201d2cb507
Bianca <ghostekkjk@gmail.com>
2025-03-31 08:41:36 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Switch_Case/ativsc4.js
@@ -0,0 +1,26 @@
+let nota = 4;
+
+switch (nota){
+    case 9:
+    case 10:
+        console.log('Nota A!');
+        break;
+    case 7: 
+    case 8:
+        console.log('Nota B!');
+        break;
+    case 5:
+    case 6:
+        console.log('Nota C.');
+        break;
+    case 3:
+    case 4:
+        console.log('Nota D.');
+        break;
+    case 0:
+    case 2:
+        console.log('Nota E.');
+        break;
+    default:
+        console.log('Nota inválida.');
+}
